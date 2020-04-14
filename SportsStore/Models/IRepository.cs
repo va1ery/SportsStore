f43abf533c599772a02c7c00decb58bae9070c65
Valery Gurov <va1ery@ya.ru>
2020-04-14 08:58:55 +0300

    Добавьте файлы проекта.

---
+++ SportsStore/Models/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace SportsStore.Models
+{
+
+    public interface IRepository
+    {
+
+        IEnumerable<Product> Products { get; }
+
+        void AddProduct(Product product);
+    }
+}
